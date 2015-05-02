9a7d3262fc4e0e9f750294dea66ce366bd4233fd
kwketh <keth@kwketh.com>
2015-05-02 17:05:15 +0100

    deleted no longer used vars in dev-env.sh

--- dev-env.sh
+++ dev-env.sh
@@ -4,8 +4,6 @@ IMAGE_NAME=kwketh/dev-env
 TAG=latest
 DOCKER=docker
 IMAGE=$IMAGE_NAME:$TAG
-DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-HASH=$(cat /dev/urandom | head -c 256 | md5 | head -c 8)
 if [ -z "$1" ]; then
     MOUNTPOINT=$(pwd)
 else

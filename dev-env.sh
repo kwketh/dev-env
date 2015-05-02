20819bf719ce3eb3ef89dd777c904f829416e97e
kwketh <keth@kwketh.com>
2015-05-02 18:20:14 +0100

    dev-env.sh now supports mounting current directory in read-only mode with -r flag, additionally allows mutable flag allowing to modify the files (outside host machine), however they will be discarded at the end of the zsh session

--- dev-env.sh
+++ dev-env.sh
@@ -4,10 +4,52 @@ IMAGE_NAME=kwketh/dev-env
 TAG=latest
 DOCKER=docker
 IMAGE=$IMAGE_NAME:$TAG
-if [ -z "$1" ]; then
-    MOUNTPOINT=$(pwd)
+WORKDIR=$1
+SESSIONID=$(cat /dev/urandom | head -c 256 | md5 | head -c 8)
+MOUNTFLAGS=""
+
+# Set default flags
+readonly='false'
+mutable='false'
+
+# Parse flags
+while getopts 'rm' flag; do
+  case "${flag}" in
+    r) readonly='true'; WORKDIR=$2 ;;
+    m) if [ "$readonly" = "true" ]; then
+         mutable=true
+       else
+         echo "error: mutable option can only be used in read-only mode."
+         exit
+       fi
+       ;;
+  esac
+done
+
+# Convert work directory to full absolute path
+if [ -z "$WORKDIR" ]; then
+    WORKDIR=$(pwd)
 else
-    MOUNTPOINT=$(realpath $1)
+    WORKDIR=$(realpath $WORKDIR)
 fi
-docker run -h $HOSTNAME --rm -it -w "/mnt" -v "$MOUNTPOINT:/mnt" $IMAGE
+
+if [ "$readonly" = "true" ]; then
+    echo "dev-env: running in read-only mode."
+    MOUNTFLAGS=":ro"
+fi
+
+if [ "$mutable" = "true" ]; then
+    SESSIONDIR="$HOME/.tmp_$SESSIONID"
+    echo "         (files are mutable but will be discarded)"
+    rsync -a $WORKDIR/ $SESSIONDIR
+    WORKDIR="$SESSIONDIR"
+    MOUNTFLAGS=""
+fi
+
+docker run -h $HOSTNAME --rm -it -w "/mnt" -v "$WORKDIR:/mnt$MOUNTFLAGS" $IMAGE
+
+if [ "$mutable" = "true" ]; then
+    echo "discarding temporary files."
+    rm -rf "$SESSIONDIR"
+fi
 

2b3ea437a9c8f3795fe52da15c7e77f792f86acf
kwketh <keth@kwketh.com>
2015-05-25 21:46:53 +0100

    disabled auto update for zsh (seems redundant since the tools are normally up-to-date)

--- sh/03_setup_zsh.sh
+++ sh/03_setup_zsh.sh
@@ -1,1 +1,4 @@
 apt-get install -y zsh && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# Disable auto updates
+echo "DISABLE_AUTO_UPDATE=\"true\"" >> ~/.zshrc

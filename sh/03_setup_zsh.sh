884facbaf4b6c526587353b498b5b3b8e903e613
Konrad W <keth@kwketh.com>
2018-04-08 21:25:10 +0100

    fixed zsh setup

--- sh/03_setup_zsh.sh
+++ sh/03_setup_zsh.sh
@@ -1,4 +1,4 @@
-apt-get install -y zsh && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+apt-get install -y zsh && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
 # Disable auto updates
 echo "DISABLE_AUTO_UPDATE=\"true\"" >> ~/.zshrc
